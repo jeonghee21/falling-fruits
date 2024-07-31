81ea902eb656946296792c89f0de1120630836a4
jeonghee21 <105273326+jeonghee21@users.noreply.github.com>
2024-07-31 15:02:11 +0900

    committ and push

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const fruitContainer = document.getElementById('fruit-container');
+const fruits = ['apple.png', 'banana.png', 'orange.png', 'tomato.png'];
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+
+function createFruit() {
+    const fruit = document.createElement('img');
+    frunit.src = 'images/' + fruits[getRandomInt(0, fruits.length)];
+    fruit.className = 'fruit';
+    fruit.style.left = getRandomInt(0, window.innerWidth - 50) + 'px';
+    fruit.style.top = '-50px';
+    fruitContainer.appendChild(fruit);
+
+    let fallSpeed = getRandomInt(2, 5);
+
+    function fall() {
+        let topPosition = parseInt(fruit.style.top);
+        if (topPosition < window.innerHeight) {
+            fruit.style.top = (topPosition + fallSpeed) + 'px';
+            requestAnimationFrame(fall);
+        } else {
+            fruit.remove();
+        }
+    }
+
+    fall();
+}
+
+setInterval(createFruit, 1000);
